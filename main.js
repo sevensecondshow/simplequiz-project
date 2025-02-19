f92b4463fd2e9d8bb20438e1df0bbf46e7e78ec7
sevensecondshow <63014456+sevensecondshow@users.noreply.github.com>
2025-02-18 23:22:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ const questionEl = document.querySelector('.question');
 const option1 = document.querySelector('.option1');
 const option2 = document.querySelector('.option2');
 const option3 = document.querySelector('.option3');
+const option4 = document.querySelector('.option4');
 const nextButton = document.querySelector('.next');
 const previousButton = document.querySelector('.previous');
 const restartButton = document.querySelector('.restart');
@@ -37,14 +38,17 @@ function generateQuestions (index) {
     const option1Total = questions[index].answer1Total;
     const option2Total = questions[index].answer2Total;
     const option3Total = questions[index].answer3Total;
+    const option4Total = questions[index].answer4Total;
     //Populate html elements 
     questionEl.innerHTML = `${index + 1}. ${question.question}`
     option1.setAttribute('data-total', `${option1Total}`);
     option2.setAttribute('data-total', `${option2Total}`);
     option3.setAttribute('data-total', `${option3Total}`);
+    option3.setAttribute('data-total', `${option4Total}`);
     option1.innerHTML = `${question.answer1}`
     option2.innerHTML = `${question.answer2}`
     option3.innerHTML = `${question.answer3}`
+    option3.innerHTML = `${question.answer4}`
 }
 
 
