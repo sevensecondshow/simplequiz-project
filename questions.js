63586915e8ca1306e941bb9a6613f4b5b00dd2f6
sevensecondshow <63014456+sevensecondshow@users.noreply.github.com>
2025-02-18 23:19:36 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,50 +1,60 @@
 const questions = [
   {
-    "question": "ASL?",
-    "answer1": "yikes",
+    "question": "It's your friend's birthday! What are you getting them?",
+    "answer1": "drinks and snacks!",
     "answer1Total": "1",
-    "answer2": "jenkies",
+    "answer2": "a personal gift that represents our friendship",
     "answer2Total": "2",
-    "answer3": "bonkers",
-    "answer3Total": "3"
+    "answer3": "nothing",
+    "answer3Total": "3",
+    "answer4": "something practical they need at home",
+    "answer4Total": "4"
   },
   {
-    "question": "i got a big wiener",
-    "answer1": "Agree",
+    "question": "It's Friday night, you're going out. How do you prepare?",
+    "answer1": "get ready and look reeeeeeeeeeal good",
     "answer1Total": "1",
-    "answer2": "Neutral",
+    "answer2": "same way I get dressed in the morning",
     "answer2Total": "2",
-    "answer3": "Disagree",
-    "answer3Total": "3"
+    "answer3": "hahaha no i don't go out",
+    "answer3Total": "3",
+    "answer4": "pregame",
+    "answer4Total": "4"
   },
   {
     "question":
-      "fuck marry kill: naruto, one piece guy, bleach guy",
-    "answer1": "naruto",
+      "Your friend is asking for advice!",
+    "answer1": "give them the answer they need",
     "answer1Total": "1",
-    "answer2": "moneky",
+    "answer2": "listen and nod and offer sympathy",
     "answer2Total": "3",
-    "answer3": "bleach",
-    "answer3Total": "2"
+    "answer3": "ask what they're looking for while talking to you",
+    "answer3Total": "2",
+    "answer4": "give them a related anecdote in return",
+    "answer4Total": "4"
   },
   {
-    "question": "Best Sentence to describe you?",
+    "question": "Your friend is getting in a fight. What's the move?",
     "answer1": "You feel superior to other people.",
     "answer1Total": "3",
     "answer2": "You consider yourself more practical than creative.",
     "answer2Total": "2",
     "answer3":
       "Winning a debate matters less to you than making sure no one gets upset.",
-    "answer3Total": "1"
+    "answer3Total": "1",
+    "answer4": "Buy them a treat",
+    "answer4Total": "4"
   },
   {
-    "question": "best sentence to describe me",
-    "answer1": "ethereal",
+    "question": "Your pal is going through a breakup.",
+    "answer1": "Be empathetic to both sides",
     "answer1Total": "1",
-    "answer2": "effervescent",
+    "answer2": "Offer to kill their ex-partner",
     "answer2Total": "2",
-    "answer3": "egregious",
-    "answer3Total": "3"
+    "answer3": "Act like things are normal",
+    "answer3Total": "3",
+    "answer4": "Buy them a treat",
+    "answer4Total": "4"
   },
   {
     "question":
@@ -55,7 +65,9 @@ const questions = [
     "answer2": "Feel hurt because you were looking forward to tonight.",
     "answer2Total": "2",
     "answer3": "No problem, you kinda wanted to stay home anyway.",
-    "answer3Total": "1"
+    "answer3Total": "1",
+    "answer4": "Guilt them into coming out anyways",
+    "answer4Total": "4"
   },
   {
     "question": "Which of the following colours do you like most?",
@@ -64,6 +76,9 @@ const questions = [
     "answer2": "brown",
     "answer2Total": "2",
     "answer3": "yellow",
-    "answer3Total": "3"
+    "answer3Total": "3",
+    "answer4": "purple",
+    "answer4Total": "4"
+    
   }
 ]
