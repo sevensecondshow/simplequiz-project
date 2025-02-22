570df34ce738d976ccd6ddc04ae892d61f8d89c8
sevensecondshow <63014456+sevensecondshow@users.noreply.github.com>
2025-02-21 23:40:37 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,82 +2,82 @@ const questions = [
   {
     "question": "It's your friend's birthday! What are you getting them?",
     "answer1": "drinks and snacks!",
-    "answer1Total": "1",
+    "answer1Total": "4",
     "answer2": "a personal gift that represents our friendship",
-    "answer2Total": "2",
+    "answer2Total": "1",
     "answer3": "nothing",
-    "answer3Total": "3",
+    "answer3Total": "2",
     "answer4": "something practical they need at home",
-    "answer4Total": "4"
+    "answer4Total": "3"
   },
   {
     "question": "It's Friday night, you're going out. How do you prepare?",
     "answer1": "get ready and look reeeeeeeeeeal good",
-    "answer1Total": "1",
+    "answer1Total": "4",
     "answer2": "same way I get dressed in the morning",
-    "answer2Total": "2",
+    "answer2Total": "3",
     "answer3": "hahaha no i don't go out",
-    "answer3Total": "3",
+    "answer3Total": "1",
     "answer4": "pregame",
-    "answer4Total": "4"
+    "answer4Total": "2"
   },
   {
     "question":
       "Your friend is asking for advice!",
     "answer1": "give them the answer they need",
-    "answer1Total": "1",
+    "answer1Total": "4",
     "answer2": "listen and nod and offer sympathy",
     "answer2Total": "3",
     "answer3": "ask what they're looking for while talking to you",
-    "answer3Total": "2",
+    "answer3Total": "1",
     "answer4": "give them a related anecdote in return",
-    "answer4Total": "4"
+    "answer4Total": "2"
   },
   {
     "question": "Your friend is getting in a fight. What's the move?",
     "answer1": "you're their second if they die!!",
-    "answer1Total": "3",
+    "answer1Total": "2",
     "answer2": "jump in, you're the queen's consort and representative",
-    "answer2Total": "2",
+    "answer2Total": "4",
     "answer3": "okay but convince them not to fight",
     "answer3Total": "1",
     "answer4": "make the first move DISTRACT and run away",
-    "answer4Total": "4"
+    "answer4Total": "3"
   },
   {
     "question": "Your pal is going through a breakup.",
     "answer1": "Be empathetic to both sides",
-    "answer1Total": "1",
+    "answer1Total": "3",
     "answer2": "Offer to kill their ex-partner",
     "answer2Total": "2",
     "answer3": "Act like things are normal",
-    "answer3Total": "3",
+    "answer3Total": "4",
     "answer4": "Buy them a treat",
-    "answer4Total": "4"
+    "answer4Total": "1"
   },
   {
     "question":
       "You make plans with a friend and they cancel on you, what do you do?",
     "answer1":
       "Say \"whatever\" and plan a night that'll be GREAT so they don't cancel again.",
-    "answer1Total": "3",
+    "answer1Total": "2",
     "answer2": "Feel hurt because you were looking forward to tonight.",
-    "answer2Total": "2",
+    "answer2Total": "1",
     "answer3": "No problem, you kinda wanted to stay home anyway.",
-    "answer3Total": "1",
+    "answer3Total": "3",
     "answer4": "Guilt them into coming out anyways",
     "answer4Total": "4"
   },
   {
     "question": "Which of the following colours do you like most?",
     "answer1": "grey",
-    "answer1Total": "1",
+    "answer1Total": "3",
     "answer2": "brown",
-    "answer2Total": "2",
+    "answer2Total": "4",
     "answer3": "magenta",
-    "answer3Total": "3",
+    "answer3Total": "1",
     "answer4": "purple",
-    "answer4Total": "4"
+    "answer4Total": "2"
     
   }
 ]
